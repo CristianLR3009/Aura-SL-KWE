01c9cb1b73bb77e2797394c0fe6ebddde382ac4e
Cristian <cristianlr.2025@outlook.com>
2024-08-28 12:44:24 -0600

    Mostrar cursos en un datatable

--- force-app/main/default/aura/cursos_completados/cursos_completadosController.js
+++ force-app/main/default/aura/cursos_completados/cursos_completadosController.js
@@ -7,7 +7,28 @@
         action.setCallback(this, function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
-                component.set("v.cursosCompletados", response.getReturnValue());
+
+                var cursos = response.getReturnValue();
+                //component.set("v.cursosCompletados", cursos);
+
+                var cursosConNombre = cursos.map(curso => {
+                    return {
+                        Id: curso.Id,
+                        Name: curso.Curso__r.Name,
+                        Fecha_de_inicio__c: curso.Fecha_de_inicio__c,
+                        Fecha_de_finalizaci_n__c: curso.Fecha_de_finalizaci_n__c
+                    }
+                });
+
+                component.set("v.cursosCompletados", cursosConNombre);
+
+                // Definir columnas para el datatable
+                component.set("v.columns", [
+                    { label: 'Nombre del Curso', fieldName: 'Name', type: 'text' },
+                    { label: 'Fecha de Inicio', fieldName: 'Fecha_de_inicio__c', type: 'date' },
+                    { label: 'Fecha de Finalización', fieldName: 'Fecha_de_finalizaci_n__c', type: 'date' },
+                ]);
+
             } else if (state === "ERROR") {
                 var errors = response.getError();
                 var message = "Error desconocido";
